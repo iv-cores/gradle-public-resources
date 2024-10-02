41d804466bf2de4af7fffb3f14f639f1921378ff
isaiah.v <isaiah.v@comcast.net>
2024-10-01 22:08:36 -0700

    added sonar properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,16 @@ sonarqube {
         property("sonar.host.url", System.getenv("SONAR_HOST_URL") ?: throw IllegalStateException("SONAR_HOST_URL is not set"))
         property("sonar.login", System.getenv("SONAR_LOGIN") ?: throw IllegalStateException("SONAR_LOGIN is not set"))
         property("sonar.coverage.jacoco.xmlReportPaths", "${layout.buildDirectory.get()}/reports/jacoco/test/jacocoTestReport.xml")
+
+        System.getProperty("sonar.scm.revision")?.let {
+            property("sonar.scm.revision", it)
+        }
+        System.getProperty("sonar.links.scm")?.let {
+            property("sonar.links.scm", it)
+        }
+        System.getProperty("sonar.links.ci")?.let {
+            property("sonar.links.ci", it)
+        }
     }
 }
 
